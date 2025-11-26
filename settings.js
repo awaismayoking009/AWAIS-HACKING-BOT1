4537408d353650852d1221c32a098446f46a5a4d
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-11-26 04:30:51 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'AWAIS BOT',
+  author: '‎',
+  botName: "AWAIS-HACKING-BOT",
+  botOwner: 'AwaisMayo', // Your name
+  ownerNumber: '923040488788', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.4",
+  updateZipUrl: "https://github.com/awaismayoking009/AWAIS-HACKING-BOT/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
